8fde65d567307a0ed35ab88db25c0d5936fbc75c
Neonx <fabiangomes2015@gmail.com>
2023-02-03 12:59:37 -0500

    Add Hamburguer.js and Add Side bar menu

---
+++ js/Hamburguer.js
@@ -0,0 +1,11 @@
+const menu = document.querySelector('#hamburguer')
+
+// menu.addEventListener("click", menuToggle)
+
+// function menuToggle() {
+//     console.log("hoalghet");
+// }
+
+menu.addEventListener("click", () => {
+    console.log("añlskdaslñk")
+})
