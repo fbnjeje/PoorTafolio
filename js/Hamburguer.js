51762ac4aeea697ca59c6017ba5d33e5eda603d8
Neonx <fabiangomes2015@gmail.com>
2023-02-04 20:03:58 -0500

    Fix side bar, now working

--- js/Hamburguer.js
+++ js/Hamburguer.js
@@ -1,11 +1,17 @@
 const menu = document.querySelector('#hamburguer')
+const menuHamburguer = document.querySelector('#menu')
+const menuX = document.querySelector("#x")
 
-// menu.addEventListener("click", menuToggle)
 
-// function menuToggle() {
-//     console.log("hoalghet");
-// }
+const sepiaFilter = document.querySelector("#blackGround")
+
 
 menu.addEventListener("click", () => {
-    console.log("añlskdaslñk")
+    menuHamburguer.classList.toggle("menu-toggle")
+    sepiaFilter.classList.toggle("blackGround")
+})
+
+menuX.addEventListener("click", () => {
+    menuHamburguer.classList.toggle("menu-toggle")
+    sepiaFilter.classList.toggle("blackGround")
 })
