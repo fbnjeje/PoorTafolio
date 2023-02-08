7082ded1d36c12c28aa91047a3c83b46f960c63b
Neonx <fabiangomes2015@gmail.com>
2023-02-08 15:47:29 -0500

    Delete intereses part

--- js/Hamburguer.js
+++ js/Hamburguer.js
@@ -15,3 +15,16 @@ menuX.addEventListener("click", () => {
     menuHamburguer.classList.toggle("menu-toggle")
     sepiaFilter.classList.toggle("blackGround")
 })
+
+
+const URL = "https://api.escuelajs.co/api/v1/products"
+
+fetch(URL)
+    .then(response => response.json())
+    .then(data => mostrarData(data))
+    .catch(err => console.log("error", err))
+
+const mostrarData = (data) => {
+    console.log(data);
+}
+
